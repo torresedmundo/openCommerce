bdab9b12f9fe0731743f775b42f912e2043cde06
Edmundo Torres <torresedmundo@gmail.com>
2022-05-09 23:50:02 -0400

    Se mejoran las funciones, DOM, Eventos, localStorage y JSON

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-
 let baseDatos = [];
 let carroCompras = [];
+let bdLocal =[];
+
+if (localStorage.length > 0) {
+    extraerLocal();
+    baseDatos = bdLocal;
+ }
 
 // AGREGAR AL INVENTARIO
 let btnAgregar = document.getElementById("botonAgregarInventario");
@@ -24,14 +29,28 @@ function Inventario (){
     let idProducto = Date.now();
     nuevoProducto = new Producto (ingNombreProducto, ingModeloProducto, ingStock, ingPrecioProducto, idProducto);
     baseDatos.push(nuevoProducto);
+    guardarLocal();
+}
+
+function guardarLocal(){
+    console.log(baseDatos);
+    guardarJSON = JSON.stringify(baseDatos);
+    console.log(guardarJSON);
+    localStorage.setItem('bdLocal', guardarJSON)
+    extraerLocal();
+}
+
+function extraerLocal(){
+    bdLocal = JSON.parse(localStorage.getItem('bdLocal'));
     mostrarInventario();
 }
-console.log(baseDatos);
+
 
 function mostrarInventario(){
+    console.log(bdLocal);
     let mostrarProductos = document.getElementById("listadoInventario");
     mostrarProductos.innerHTML = ""; 
-    for (let indice of baseDatos){
+    for (let indice of bdLocal){
         mostrarProductos.innerHTML += 
             `<div id = "${indice.idProducto}" class="container col-md-12 d-flex justify-content-center">
                 <div class="d-flex col-md-3">
@@ -65,41 +84,44 @@ function EliminarInventario () {
     let ingCantEliminar = document.getElementById("InputCantEliminar").value;
     console.log(ingIdProducto);
     console.log(ingCantEliminar);
-    if (baseDatos.some((el) => el.idProducto == ingIdProducto) != true) {
+    if (bdLocal.some((el) => el.idProducto == ingIdProducto) != true) {
         console.log('Producto NO Existe en Inventario');
-        console.log()
      } else
         {
-        let cantidad = baseDatos.find((el) => el.idProducto == ingIdProducto);
-        let indice = baseDatos.findIndex((el) => el.idProducto == ingIdProducto)
+        let cantidad = bdLocal.find((el) => el.idProducto == ingIdProducto);
+        let indice = bdLocal.findIndex((el) => el.idProducto == ingIdProducto)
         resultadoStock = cantidad.stock - ingCantEliminar;
         console.log (resultadoStock);
         console.log (indice);
         if (resultadoStock <= 0){
             console.log('No hay Stock suficiente para compra  - BORRAR DEL INVENTARIO');
-            baseDatos.splice( 
-                baseDatos.findIndex((el) => el.idProducto == ingIdProducto), 1);
-                console.log(baseDatos);
+            bdLocal.splice( 
+                bdLocal.findIndex((el) => el.idProducto == ingIdProducto), 1);
+                console.log(bdLocal);
+                baseDatos = bdLocal;
+                guardarLocal();
+                //localStorage.removeItem('bdLocal');
                 mostrarInventario();
             }  else
             {
                 console.log('Stock Disponible - SEGUIR MOSTRANDO INVENTARIO');
-                let actualizar = baseDatos.find((el) => el.idProducto == ingIdProducto);
+                let actualizar = bdLocal.find((el) => el.idProducto == ingIdProducto);
                 actualizar.stock = resultadoStock;
-                console.log(baseDatos);
+                console.log(bdLocal);
+                baseDatos = bdLocal;
+                guardarLocal();
                 mostrarInventario();
             }        
         }
+    }
+    
+function eliminarLocal (){
+
  }
     
 
-
-    
-
-
-
 /*
-PARA VISTA DE CLIENTE
+IDEAS - PARA VISTA DE CLIENTE
 validarProducto ();
 // Funcion para agregar al carro
 function validarProducto(){
@@ -149,4 +171,4 @@ let nombre = document.getElementById("nombreProducto");
 nombre.innerText = (ProductosCarro.nombreProducto);
 console.log(nombreProducto[0]);
 
- */
+*/
